71c7a1b3415bc09548f28bbb31fc931e7a0773e7
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-05 16:27:21 +0300

    Primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/nekta/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,16 @@
+package com.nekta.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+
+//we cant use more than one primary cause we will have problem. we can use only one primary
+//and the qualifier annotation is always first than primary
+@Component
+public class CricketCoach implements Coach {
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes!!!!!";
+    }
+}
