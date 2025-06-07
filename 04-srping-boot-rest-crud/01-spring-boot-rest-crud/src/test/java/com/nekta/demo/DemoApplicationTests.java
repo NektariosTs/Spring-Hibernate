c76d86c2b5a345579abeeb9ed51d0e2fa540359d
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-07 18:54:08 +0300

    RestController/HandleException

---
+++ 04-srping-boot-rest-crud/01-spring-boot-rest-crud/src/test/java/com/nekta/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekta.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
