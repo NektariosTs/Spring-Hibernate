457f1891f7cfeabc2f25519a929403e1d66a2f89
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-11 14:19:48 +0300

    first commit with JpaRepository

---
+++ 04-srping-boot-rest-crud/03-spirng-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/nekta/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.nekta.springboot.cruddemo.service;
+
+import com.nekta.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
