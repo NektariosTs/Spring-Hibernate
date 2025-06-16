a996858ebe6d13774ddf50b6481b87ed59142b46
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-16 20:03:49 +0300

    spring MVC security

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/nekta/springboot/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekta.springboot.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
