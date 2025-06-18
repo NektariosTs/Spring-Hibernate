011afdf73478245144bd9253aebc65e622bfe173
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-18 16:20:48 +0300

    delete bi-directional one to one

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/nekta/cruddemo/dao/AppDAO.java
@@ -0,0 +1,19 @@
+package com.nekta.cruddemo.dao;
+
+
+import com.nekta.cruddemo.entity.Instructor;
+import com.nekta.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+}
