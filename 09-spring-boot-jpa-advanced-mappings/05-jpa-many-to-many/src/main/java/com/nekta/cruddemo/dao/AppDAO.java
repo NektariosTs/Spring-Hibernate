640e542267664058e18bed0236da75dfeaca6e0a
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-19 16:01:55 +0300

    Many to many

---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/nekta/cruddemo/dao/AppDAO.java
@@ -0,0 +1,40 @@
+package com.nekta.cruddemo.dao;
+
+
+import com.nekta.cruddemo.entity.Course;
+import com.nekta.cruddemo.entity.Instructor;
+import com.nekta.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+    Course findCourseAndStudentsByCourseId(int theId);
+
+}
