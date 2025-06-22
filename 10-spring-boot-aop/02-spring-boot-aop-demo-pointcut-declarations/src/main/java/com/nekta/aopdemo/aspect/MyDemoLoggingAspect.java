4e8edce24a68875b35dc961f27a4eaf8c38704d8
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-06-22 20:25:57 +0300

    @Order the advices

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/nekta/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/nekta/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package com.nekta.aopdemo.aspect;
 
+import com.nekta.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +15,44 @@ public class MyDemoLoggingAspect {
 
 
     @Before("com.nekta.aopdemo.aspect.NektaAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n======>>> Executing @Before advice on method()");
+
+        //display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        //display method arguments
+
+        //get args
+        Object[] args = theJoinPoint.getArgs();
+
+
+        //loop through args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+
+                //downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
